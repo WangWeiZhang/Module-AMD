07e7a72f89ee9ad9493677eb63e2a70dcfe142da
WangWeiZhang <352875258@qq.com>
2017-07-02 23:08:24 +0800

    自定义模块define

---
+++ Module-AMD-Study/response/app/uitl.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+define(['jquery'],function($){
+    return {
+        change:function(){
+            $("body").css({"backgroundColor":"#f00"});
+        },
+        show:function(){
+            alert("王维璋")
+        },
+        message:function(){
+            alert("www.www.com");
+        }
+    }
+});
